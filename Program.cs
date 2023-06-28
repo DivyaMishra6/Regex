9ee117c43033aaf544f1e60649cce7df17825dbc
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-29 00:19:07 +0530

    [Divya]:Regex Expression

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "abbb";
+
+
+            string pattern = "^a{1}b{2,3}$";
+
+
+            Regex regex = new Regex(pattern);
+
+
+            if (regex.IsMatch(input))
+            {
+                Console.WriteLine("String matched the pattern!");
+            }
+            else
+            {
+                Console.WriteLine("String did not match the pattern!");
+            }
+
+        }
+    }
+
+}
